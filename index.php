48d69b753ce992eb50cdb90a3c8e3be1f324628f
YamadaTV <liam.mattsson@gymnasium.ax>
2017-01-19 12:55:06 +0200

    Skapade en startsida med formulär efter namn

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to Chatbot!</title>
+</head>
+<body>
+
+<?php if ($_GET['name'] == NULL): ?>
+
+<form>
+	<label for="name">Name</label>
+    <input type="text" name="name" id="name">
+    <input type="submit" value="Send!">
+</form>
+
+<?php endif ?>
+
+</body>
+</html>
